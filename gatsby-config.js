9f649333d3cc142f7ab43340cf64373f02104934
Armstrong <xuting0510@gmail.com>
2023-08-20 21:16:42 +0800

    fix: update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Armstrong',
     description:
-      'Armstrong is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Armstrong is a product innovator in the renewable energy and power sector, focusing on creating advanced solutions.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
